6f0f4e69f029dcfc7b3980bebf5158f84a3b31b4
Sara Hatem <Sara.Hatem@bnsights.com>
2022-11-01 16:27:01 +0200

    style: Remove comments and extra lines.

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -9,7 +9,7 @@ namespace WebApplication1.Controllers
     public class HomeController : Controller
     {
         private static readonly string[] Summaries = new[]
-       {
+        {
             "Freezing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
         };
 
@@ -28,22 +28,15 @@ namespace WebApplication1.Controllers
         public ActionResult Index()
         {
             var model = Get();
-
             return View(model);
         }
-
-
     }
-
 
     public class WeatherForecast
     {
         public DateTime Date { get; set; }
-
         public int TemperatureC { get; set; }
-
         public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
-
         public string Summary { get; set; }
     }
 }
